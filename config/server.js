7e460fc87b5badcc983ef5faed03b454ae3d1782
Eduardo Paixão <duduindo@gmail.com>
2017-09-24 18:19:44 -0300

    Added module express-validator(middleware)

--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
 
 const app = express();
 app.set('view engine', 'ejs'); // Engine of views
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true})); // Middleware of application
+app.use(expressValidator());
 
 consign()
 	.include('app/routes')
