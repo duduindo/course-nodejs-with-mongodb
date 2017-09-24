99a9341909d309856b3222953c239c61e11875c0
Eduardo Paixão <duduindo@gmail.com>
2017-09-24 14:32:16 -0300

    Installed and configured body-parser(middleware)

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,5 +1,13 @@
+
+
 module.exports = app => {
 	app.get('/formulario_inclusao_noticia', (req, res) => {
 		res.render('./admin/form_add_noticia');
+	});
+
+	app.post('/noticias/salvar', (req, res) => {
+		const noticias = req.body;
+
+		res.send(noticias);
 	});
 };
--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs'); // Engine of views
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true})); // Middleware of application
 
 consign()
 	.include('app/routes')
