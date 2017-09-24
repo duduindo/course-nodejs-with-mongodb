1d774dac8a1f0eefdd8f5ecbba92a614ace1bb28
Eduardo Paixão <duduindo@gmail.com>
2017-09-24 11:57:48 -0300

    Consign installed and configured

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const app = require('./config/server');
 
-const rotaHome = require('./app/routes/home')(app);
+//const rotaHome = require('./app/routes/home')(app);
 
-const rotaNoticias = require('./app/routes/noticias')(app);
+//const rotaNoticias = require('./app/routes/noticias')(app);
 
-const rotaForm = require('./app/routes/formulario_inclusao_noticia')(app);
+//const rotaForm = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, () => {
 	console.log("Servidor ON");
