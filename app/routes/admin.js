7b778588902cbcda8bd64924586344452689e723
Eduardo Paixão <duduindo@gmail.com>
2017-09-24 15:01:39 -0300

    Configured send form to MySQL

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,18 @@
 
 
-module.exports = app => {
-	app.get('/formulario_inclusao_noticia', (req, res) => {
+module.exports = application => {
+	application.get('/formulario_inclusao_noticia', (req, res) => {
 		res.render('./admin/form_add_noticia');
 	});
 
-	app.post('/noticias/salvar', (req, res) => {
-		const noticias = req.body;
-
-		res.send(noticias);
+	application.post('/noticias/salvar', (req, res) => {
+		const noticia = req.body;
+				
+		const connection = application.config.dbConnection();
+		const { noticiasModel } = application.app.models;		
+		
+		noticiasModel.salvarNoticia(noticia, connection, (error, result) => {
+			res.redirect('/noticias');
+		});	
 	});
 };
