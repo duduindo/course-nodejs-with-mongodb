3cb34e733d9925c0344fdbfad56c5e3b64440aee
Eduardo Paixão <duduindo@gmail.com>
2017-09-23 23:40:26 -0300

    14º Lesson done

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send("<html><body>Portal de noticias</body></html>");
-});
+app.set('view engine', 'ejs'); // Engine of views
 
 app.get('/tecnologia', (req, res) => {
-	res.send("<html><body>Noticias de tecnologia</body></html>");
+	res.render('./secao/tecnologia');
 });
+
+app.get('/', (req, res) => {
+	res.send('<html><body>Portal de noticias</body></html>');
+});
+
 
 app.listen(3000, () => {
 	console.log('servidor rodando');
